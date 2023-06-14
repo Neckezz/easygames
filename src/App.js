0a80921d4b7d7a919769f082545a81f5352690fd
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-06-14 17:53:50 +0200

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
-
+import Header from './components/header'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return ( 
+        <header>
+            <a href=""><img src="" alt="EasyGames"id="logo" /></a>
+            <ul>
+                <li><a href=""><img src="./images/account.png" alt="Account" /></a></li>
+                <li><a href=""><img src="./images/shopping-cart.png" alt="Cart" /></a></li>
+            </ul>
+        </header>
+     );
+}
+ 
+export default Header;
