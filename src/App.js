2132a0242a30d4c5a4f54414a95c9b9ebb26c498
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-06-23 17:18:53 +0200

    Finished newsarticle component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/header'
+import NewArticles from './components/newarticles';
 function App() {
   return (
     <div className="App">
       <Header />
+      <NewArticles/>
     </div>
   );
 }
--- src/components/newarticles.js
+++ src/components/newarticles.js
@@ -1,6 +1,15 @@
+import baner1 from '../images/baner1.png';
+import baner2 from '../images/baner2.png';
+import racunari from '../images/racunar.png';
 const NewArticles = () => {
     return ( 
-        <h1>Hello world</h1>
+        <div className="news">
+            <a href=""><img id="pc" src={racunari} alt="racunari" /></a>
+            <div className="banner">
+            <a href=""><img src={baner1} alt="baner1" /></a>
+            <a href=""><img src={baner2} alt="baner2" /></a>
+            </div>
+        </div>
      );
 }
  
