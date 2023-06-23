9b89d47caf149630ad2804e889896351328e94fb
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-06-23 17:30:46 +0200

    minor bug with navigation

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,12 +1,12 @@
 const Categories = () => {
     return ( 
         <nav>
+          <li><a href="">Consoles and PC</a></li>
           <li><a href="">Playstation 5 games</a></li>
           <li><a href="">Playstation 4 games</a></li>
           <li><a href="">Xbox games</a></li>
           <li><a href="">Nintendo Switch</a></li>
           <li><a href="">PC Games</a></li>
-          <li><a href="">Consoles and PC</a></li>
         </nav>
      );
 }
