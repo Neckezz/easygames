86c745f38aefb760af5148a1aadb704f71b03806
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-06-28 18:59:28 +0200

    preorder component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from './components/header'
 import NewArticles from './components/newarticles';
+import Preorder from './components/preorder';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
       <NewArticles/>
+      <Preorder/>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/preorder.js
@@ -0,0 +1,13 @@
+import product from "../components/Database"
+import { useEffect,useState } from "react";
+
+const Preorder = () => {
+    useEffect(()=>{
+
+    })
+    return ( 
+        <h1>Hello World</h1>
+     );
+}
+ 
+export default Preorder;
