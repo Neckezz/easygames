07cf46caba354661a6058c85e964112a89248fb5
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-01 17:19:28 +0200

    Fetch products by ID

---
+++ src/components/fetchID.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const FetchID = ({ productId }) => {
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/product/${productId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching product:", error);
+      });
+  }, [productId]);
+
+  return (
+    <div>
+      {product ? (
+        <div className="item">
+          <h1>{product.title}</h1>
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default FetchID;
