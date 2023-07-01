4af29b08a439f04d1c7a38468780c2fe29938529
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-01 16:53:49 +0200

    navigate through our products (not finished)

---
+++ src/components/products.js
@@ -0,0 +1,12 @@
+import Categories from "./categories";
+const Products = () => {
+    return (
+        <div className="products">
+            <h1>Shop right now</h1>
+            <h4>Experience gaming adventures for the lowest price in Serbia</h4>
+            <Categories/>
+        </div>
+     );
+}
+ 
+export default Products;
