42cb8e63ba76e41564d806ac64651f416051c2d7
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-02 22:31:43 +0200

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/header'
 import NewArticles from './components/newarticles';
 import Products from './components/products';
+import Footer from './components/footer'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <NewArticles/>
       <Products/>
+      <Footer/>
     </Router>
   );
 }
