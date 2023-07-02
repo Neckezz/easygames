d3379bbc2078ac352da45dfb1f2732fe6c947b38
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-02 21:08:10 +0200

    New minimalist product table

--- src/components/products.js
+++ src/components/products.js
@@ -1,9 +1,11 @@
 import Categories from "./categories";
 import Fetch from "./fetch"
+import pozadina from "../images/pozadina.png"
 const Products = () => {
     return (
         <div className="products">
             <div className="text">
+            <img src={pozadina} alt="razer" />
             <h1>Shop right now</h1>
             <h3>Experience gaming adventures for the lowest price in Serbia</h3>
             <Categories/>
