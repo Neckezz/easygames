4aa9fbabcc9ad20ee380487505d255f140610ae2
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-03 16:46:19 +0200

    little jsx mistakes fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,15 @@
+import paypal from "../images/paypal.png"
+import paysafe from "../images/paysafe.png"
+import visa from "../images/visa.png"
 const Footer = () => {
     return ( 
         <div className="footer">
             <div className="newsletter">
                 <h1>Subscribe to our Newsletter:</h1>
-                <input type="text" />
+                <input defaultValue={"example@gmail.com"} type="text" />
                 <button>Sumbit</button>
             </div>
             <div className="information">
-                <img src="" alt="Logo" />
                 <div className="locations">
                     <ul>
                         <h3>Visit us:</h3>
@@ -18,18 +20,19 @@ const Footer = () => {
                 </div>
                 <div className="support">
                     <h3>Need tehnical support?</h3>
-                    <h3>Contact us:</h3>
+                    <h3>Contact us 📱</h3>
                     <ul>
                         <li>+381 123 456 789</li>
                         <li>+381 987 654 321</li>
                     </ul>
                 </div>
-                <div className="socialmedia">
-                    <img src="" alt="Instagram" />
-                    <img src="" alt="Tiktok" />
-                    <img src="" alt="Youtube" />
-                </div>
             </div>
+            <div className="partners">
+                    <img src={paypal} alt="Paypal" />
+                    <img src={paysafe} alt="Paysafe" />
+                    <img src={visa} alt="Visa" />
+                </div>
+            <div className="trademark">2023©Easygames All Rights Reserved</div>
         </div>
      );
 }
