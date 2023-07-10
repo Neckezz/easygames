cbe2ee40b64727178b8d6b6f3ef7dea7ffe5bda3
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-10 14:04:15 +0200

    Added Page404

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Routes,Route} from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Page404 from "./pages/Page404";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage/>}/>
       <Route path="/createaccount"/>
       <Route path="/cart"/>
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 }
---
+++ src/pages/Page404.js
@@ -0,0 +1,12 @@
+const Page404 = () => {
+    return (
+        <div className="page404">
+            <h1>Oops!</h1>
+            <h3>404- Page not found</h3>
+            <p>The page you are looking might have been removed or had its name changed.</p>
+            <a href="/"><button>GO TO HOMEPAGE</button></a>
+        </div>
+     );
+}
+ 
+export default Page404;
