76f3f6bd2f6a88c0eaf272e067a9078481d3452d
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-15 19:13:30 +0200

    Header and footer are auto added at every page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import {Routes,Route} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Page404 from "./pages/Page404";
+import Header from "./components/header"
+import Footer from "./components/footer"
 function App() {
   return (
+    <div className="app">
+    <Header/>
     <Routes>
       <Route path="/" element={<Homepage/>}/>
+      <Route path="/video-games/:id"/>
       <Route path="/createaccount"/>
       <Route path="/cart"/>
       <Route path="*" element={<Page404 />} />
     </Routes>
+    <Footer/>
+    </div>
   );
 }
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,12 @@
-import Header from "../components/header";
 import NewArticles from "../components/newarticles";
 import Products from "../components/products";
-import Footer from "../components/footer";
 import Refundpolicy from "../components/refundpolicy";
 const Homepage = () => {
     return ( 
         <div className="homepage">
-            <Header/>
             <NewArticles/>
             <Products/>
             <Refundpolicy/>
-            <Footer/>
         </div>
      );
 }
