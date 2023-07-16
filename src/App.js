8fe4df34287990ccf411928130981d68b9b1766e
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-16 13:24:04 +0200

    New route path with dynamic id value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Routes,Route} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Page404 from "./pages/Page404";
+import ProductPage from "./pages/ProductPage";
 import Header from "./components/header"
 import Footer from "./components/footer"
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <Header/>
     <Routes>
       <Route path="/" element={<Homepage/>}/>
-      <Route path="/video-games/:id"/>
+      <Route path="/products/:id" element={<ProductPage/>}/>
       <Route path="/createaccount"/>
       <Route path="/cart"/>
       <Route path="*" element={<Page404 />} />
