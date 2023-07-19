82b2e45dc2dd1170de9c95847b1b2f9096c6ca1f
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-07-19 19:24:58 +0200

    Fetch products by their ID individually

--- src/components/fetchID.js
+++ src/components/fetchID.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import loading from "../images/loading.gif"
 
 const FetchID = ({ productId }) => {
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState([]);/* ${productId} */
   const[Loading,setLoading]=useState(true);
 
   useEffect(() => {
-    fetch(`http://localhost:8000/product/${productId}`)
+    fetch(`http://localhost:8000/product/${productId}`) 
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -26,7 +26,7 @@ const FetchID = ({ productId }) => {
         <h1>{product.title}</h1>
         <p>{product.price}</p>
         <p>{product.description}</p>
-        <img src={product.img} alt={product.title} />
+        <img src={product.img} alt={product.id} />
       </div>
     )}
     </div>
