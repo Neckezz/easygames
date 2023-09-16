2e12e1e01d24467e62d46595b5dfcc1a709905f0
Nemanja Ciric <nemanjaciric2006@gmail.com>
2023-09-16 18:26:30 +0200

    Now price changes while ++ and --

--- src/components/fetchID.js
+++ src/components/fetchID.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import loading from "../images/loading.gif";
 
 const FetchID = ({ productId }) => {
-  const [product, setProduct] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState(null);
+  const [loadingProduct, setLoadingProduct] = useState(true);
+  const [value, setValue] = useState(1);
 
   useEffect(() => {
     fetch(`http://localhost:8000/product/${productId}`)
@@ -11,46 +12,44 @@ const FetchID = ({ productId }) => {
       .then((data) => {
         console.log(data);
         setProduct(data);
-        setLoading(false);
+        setLoadingProduct(false);
       })
       .catch((error) => {
         console.error("Error fetching product:", error);
       });
   }, [productId]);
 
-   const increment = () => {
-    let element = document.getElementById("value");
-    let currentValue = parseInt(element.innerHTML);
-    currentValue++;
-    element.innerHTML = currentValue.toString();
+  const increment = () => {
+    setValue((prevValue) => prevValue + 1);
   };
-  
+
   const decrement = () => {
-      let element = document.getElementById("value");
-      let currentValue = parseInt(element.innerHTML);
-      if (currentValue > 1) {
-      currentValue--;
-      element.innerHTML = currentValue.toString();
-      }
+    if (value > 1) {
+      setValue((prevValue) => prevValue - 1);
+    }
   };
 
   return (
     <div>
-      {loading && <div className="loading"><img src={loading} alt="loading..." /></div>}
+      {loadingProduct && (
+        <div className="loading">
+          <img src={loading} alt="loading..." />
+        </div>
+      )}
       {product && (
         <div className="product" key={product.id}>
           <h1>{product.title}</h1>
           <div className="flex">
-          <img src={product.img} alt={product.id} />
+            <img src={product.img} alt={product.id} />
 
-          <div className="right">
-            <p id="description">{product.description}</p>
-            <h3>{product.price}</h3>
+            <div className="right">
+              <p id="description">{product.description}</p>
+              <h3 id="price">{product.price * value}$</h3>
               <button onClick={increment}>+</button>
-              <button id="value">1</button>
+              <button>{value}</button>
               <button onClick={decrement}>-</button>
               <button id="cart">Add to cart</button>
-          </div>
+            </div>
           </div>
         </div>
       )}
